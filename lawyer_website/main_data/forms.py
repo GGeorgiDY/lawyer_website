12a7605eb1ca119e0735563af0dd1185086b2a41
Георги Йорданов <gyordanov94@abv.bg>
2024-08-26 14:53:03 +0300

    customize the registration and the login pages

--- lawyer_website/main_data/forms.py
+++ lawyer_website/main_data/forms.py
@@ -7,6 +7,17 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
+        labels = {
+            'email': 'Email',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'password1': 'Password',
+            'password2': 'Confirm Password',
+        }
+        help_texts = {
+            'first_name': 'This field is optional.',
+            'last_name': 'This field is optional.',
+        }
 
 
 class CustomAuthenticationForm(AuthenticationForm):
